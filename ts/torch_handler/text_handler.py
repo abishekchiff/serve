72d4c77b6be2c526a3bdbfb1625ef579b63d9d55
Abishek Chiffon <abishekchiffon@gmail.com>
2020-10-07 20:27:46 +0530

    Refactored base_handler and text_classifier_kf

--- ts/torch_handler/text_handler.py
+++ ts/torch_handler/text_handler.py
@@ -6,35 +6,45 @@ Contains various text based utility methods
 """
 import os
 import re
+import logging
 import string
 import unicodedata
 from abc import ABC
+from captum.attr import LayerIntegratedGradients
 import torch
 import torch.nn.functional as F
 from torchtext.data.utils import get_tokenizer
-from torchtext.data.utils import ngrams_iterator
 from .base_handler import BaseHandler
 from .contractions import CONTRACTION_MAP
-from captum.attr import LayerIntegratedGradients, TokenReferenceBase, visualization
 
-CLEANUP_REGEX = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+logger = logging.getLogger(__name__)
+
+
+CLEANUP_REGEX = re.compile("<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});")
 CONTRACTIONS_PATTERN = re.compile(
-    '({})'.format('|'.join(CONTRACTION_MAP.keys())),
-    flags=re.IGNORECASE | re.DOTALL
+    "({})".format("|".join(CONTRACTION_MAP.keys())),
+    flags=re.IGNORECASE | re.DOTALL,
 )
+
 
 class TextHandler(BaseHandler, ABC):
     """
     Base class for all text based default handler.
     Contains various text based utility methods
     """
+
     def __init__(self):
         super(TextHandler, self).__init__()
         self.source_vocab = None
-        self.tokenizer = get_tokenizer('basic_english')
+        self.tokenizer = get_tokenizer("basic_english")
         self.input_text = None
+        self.lig = None
+        self.initialized = None
 
     def initialize(self, context):
+        """
+        Loads the model and Initializes the necessary artifacts
+        """
         super(TextHandler, self).initialize(context)
         self.initialized = False
         source_vocab = self.manifest['model']['sourceVocab'] if 'sourceVocab' in self.manifest['model'] else None
@@ -58,11 +68,18 @@ class TextHandler(BaseHandler, ABC):
                             'documentation for details on using text_handler.')
 
     def _expand_contractions(self, text):
+        """
+        Expands the contracted words in the text
+        """
+
         def expand_match(contraction):
             match = contraction.group(0)
             first_char = match[0]
-            expanded_contraction = CONTRACTION_MAP.get(match) if CONTRACTION_MAP.get(match) else CONTRACTION_MAP.get(
-                match.lower())
+            expanded_contraction = (
+                CONTRACTION_MAP.get(match)
+                if CONTRACTION_MAP.get(match)
+                else CONTRACTION_MAP.get(match.lower())
+            )
             expanded_contraction = first_char + expanded_contraction[1:]
             return expanded_contraction
 
@@ -71,11 +88,21 @@ class TextHandler(BaseHandler, ABC):
         return text
 
     def _remove_accented_characters(self, text):
-        text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+        """
+        Removes remove_accented_characters
+        """
+        text = (
+            unicodedata.normalize("NFKD", text)
+            .encode("ascii", "ignore")
+            .decode("utf-8", "ignore")
+        )
         return text
 
     def _remove_html_tags(self, text):
-        clean_text = CLEANUP_REGEX.sub('', text)
+        """
+        Removes html tags
+        """
+        clean_text = CLEANUP_REGEX.sub("", text)
         return clean_text
 
     def _remove_puncutation(self, *args, **kwargs):
@@ -85,26 +112,28 @@ class TextHandler(BaseHandler, ABC):
         return self._remove_punctuation(*args, **kwargs)
 
     def _remove_punctuation(self, text):
-        return text.translate(str.maketrans('', '', string.punctuation))
+        """
+        Removes punctuation
+        """
+        return text.translate(str.maketrans("", "", string.punctuation))
 
     def _tokenize(self, text):
         return self.tokenizer(text)
 
-    def get_word_token(self,input_ids, tokenizer):
+    def get_word_token(self, input_tokens):
         """
-        constructs word tokens from token id
+        Constructs word tokens from text
         """
-        #indices = input_ids[0].detach().tolist()
-        #tokens = [ tok for tok in ngrams_iterator(input_ids, self.ngrams)]
         # Remove unicode space character from BPE Tokeniser
-        tokens = [token.replace("Ġ", "") for token in input_ids]
+        tokens = [token.replace("Ġ", "") for token in input_tokens]
         return tokens
+
     def summarize_attributions(self, attributions):
         """
         Summarises the attribution across multiple runs
         """
         attributions = F.softmax(attributions)
         attributions_sum = attributions.sum(dim=-1)
-        print("attributions sum shape", attributions_sum.shape)
+        logger.info("attributions sum shape %d", attributions_sum.shape)
         attributions = attributions / torch.norm(attributions_sum)
         return attributions
