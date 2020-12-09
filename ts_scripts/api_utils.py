8e3714ade25f792ca3d3937ae5a6746b11cee2f3
Abishek Chiffon <abishekchiffon@gmail.com>
2020-12-09 16:59:37 +0530

    Added additional directories for kf and added https test in api_utils

--- ts_scripts/api_utils.py
+++ ts_scripts/api_utils.py
@@ -11,6 +11,9 @@ from ts_scripts import tsutils as ts
 TEST_DIR = os.path.join("test")
 MODEL_STORE_DIR = os.path.join("model_store")
 
+# Contains separate collection and respecive tests for both kfserving and torchserve execution
+
+### Torchserve
 ARTIFACTS_MANAGEMENT_DIR = os.path.join("artifacts", "management")
 ARTIFACTS_INFERENCE_DIR = os.path.join("artifacts", "inference")
 ARTIFACTS_INCRSD_TIMEOUT_INFERENCE_DIR = os.path.join("artifacts", "increased_timeout_inference")
@@ -21,14 +24,29 @@ TS_CONFIG_FILE_HTTPS = os.path.join("resources", "config.properties")
 
 POSTMAN_ENV_FILE = os.path.join("postman", "environment.json")
 POSTMAN_INFERENCE_DATA_FILE = os.path.join("postman", "inference_data.json")
-POSTMAN_INFERENCE_DATA_FILE_KF = os.path.join("postman", "kf_inference_data.json")
 POSTMAN_INCRSD_TIMEOUT_INFERENCE_DATA_FILE = os.path.join("postman", "increased_timeout_inference.json")
 
+#only one management collection for both kfserving and torchserve
 POSTMAN_COLLECTION_MANAGEMENT = os.path.join("postman", "management_api_test_collection.json")
 POSTMAN_COLLECTION_INFERENCE = os.path.join("postman", "inference_api_test_collection.json")
-POSTMAN_COLLECTION_INFERENCE_KF = os.path.join("postman", "kf_inference_api_test_collection.json")
 
 POSTMAN_COLLECTION_HTTPS = os.path.join("postman", "https_test_collection.json")
+
+
+### KFserving
+ARTIFACTS_MANAGEMENT_DIR_KF = os.path.join("artifacts", "management_kf")
+ARTIFACTS_INFERENCE_DIR_KF = os.path.join("artifacts", "inference_kf")
+ARTIFACTS_INCRSD_TIMEOUT_INFERENCE_DIR_KF = os.path.join("artifacts", "increased_timeout_inference_kf")
+ARTIFACTS_HTTPS_DIR_KF = os.path.join("artifacts", "https_kf")
+
+TS_CONFIG_FILE_HTTPS_KF = os.path.join("resources", "config_kf.properties")
+
+POSTMAN_INFERENCE_DATA_FILE_KF = os.path.join("postman", "kf_inference_data.json")
+POSTMAN_INCRSD_TIMEOUT_INFERENCE_DATA_FILE_KF = os.path.join("postman", "increased_timeout_inference.json")
+
+POSTMAN_COLLECTION_INFERENCE_KF = os.path.join("postman", "kf_inference_api_test_collection.json")
+
+POSTMAN_COLLECTION_HTTPS_KF = os.path.join("postman", "kf_https_test_collection.json")
 
 REPORT_FILE = os.path.join("report.html")
 
@@ -92,6 +110,7 @@ def trigger_https_tests():
     cleanup_model_store()
     return EXIT_CODE
 
+## KFserving tests starts here
 def trigger_management_tests_kf():
     """ Return exit code of newman execution of management collection """
     
@@ -100,9 +119,9 @@ def trigger_management_tests_kf():
     config_file.close()
 
     ts.start_torchserve(ncs=True, model_store=MODEL_STORE_DIR, config_file="config.properties", log_file=TS_CONSOLE_LOG_FILE)
-    EXIT_CODE = os.system(f"newman run -e {POSTMAN_ENV_FILE} {POSTMAN_COLLECTION_MANAGEMENT} -r cli,html --reporter-html-export {ARTIFACTS_MANAGEMENT_DIR}/{REPORT_FILE} --verbose")
+    EXIT_CODE = os.system(f"newman run -e {POSTMAN_ENV_FILE} {POSTMAN_COLLECTION_MANAGEMENT} -r cli,html --reporter-html-export {ARTIFACTS_MANAGEMENT_DIR_KF}/{REPORT_FILE} --verbose")
     ts.stop_torchserve()
-    move_logs(TS_CONSOLE_LOG_FILE, ARTIFACTS_MANAGEMENT_DIR)
+    move_logs(TS_CONSOLE_LOG_FILE, ARTIFACTS_MANAGEMENT_DIR_KF)
     cleanup_model_store()
     return EXIT_CODE
 
@@ -114,9 +133,19 @@ def trigger_inference_tests_kf():
     config_file.close()
 
     ts.start_torchserve(ncs=True, model_store=MODEL_STORE_DIR, config_file="config.properties", log_file=TS_CONSOLE_LOG_FILE)
-    EXIT_CODE = os.system(f"newman run -e {POSTMAN_ENV_FILE} {POSTMAN_COLLECTION_INFERENCE_KF} -d {POSTMAN_INFERENCE_DATA_FILE_KF} -r cli,html --reporter-html-export {ARTIFACTS_INFERENCE_DIR}/{REPORT_FILE} --verbose")
+    EXIT_CODE = os.system(f"newman run -e {POSTMAN_ENV_FILE} {POSTMAN_COLLECTION_INFERENCE_KF} -d {POSTMAN_INFERENCE_DATA_FILE_KF} -r cli,html --reporter-html-export {ARTIFACTS_INFERENCE_DIR_KF}/{REPORT_FILE} --verbose")
     ts.stop_torchserve()
-    move_logs(TS_CONSOLE_LOG_FILE, ARTIFACTS_INFERENCE_DIR)
+    move_logs(TS_CONSOLE_LOG_FILE, ARTIFACTS_INFERENCE_DIR_KF)
+    cleanup_model_store()
+    return EXIT_CODE
+
+
+def trigger_https_tests_kf():
+    """ Return exit code of newman execution of https collection """
+    ts.start_torchserve(ncs=True, model_store=MODEL_STORE_DIR, config_file=TS_CONFIG_FILE_HTTPS_KF, log_file=TS_CONSOLE_LOG_FILE)
+    EXIT_CODE = os.system(f"newman run --insecure -e {POSTMAN_ENV_FILE} {POSTMAN_COLLECTION_HTTPS_KF} -r cli,html --reporter-html-export {ARTIFACTS_HTTPS_DIR_KF}/{REPORT_FILE} --verbose")
+    ts.stop_torchserve()
+    move_logs(TS_CONSOLE_LOG_FILE, ARTIFACTS_HTTPS_DIR_KF)
     cleanup_model_store()
     return EXIT_CODE
 
@@ -128,12 +157,15 @@ def trigger_all():
     exit_code4 = trigger_https_tests()
     exit_code5 = trigger_management_tests_kf()
     exit_code6 = trigger_inference_tests_kf()
-    return 1 if any(code != 0 for code in [exit_code1, exit_code2, exit_code3, exit_code4, exit_code5, exit_code6]) else 0
+    exit_code7 = trigger_https_tests_kf()
+    return 1 if any(code != 0 for code in [exit_code1, exit_code2, exit_code3, exit_code4, exit_code5, exit_code6, exit_code7]) else 0
 
 
 def test_api(collection):
     os.chdir(TEST_DIR)
-    for DIR in [MODEL_STORE_DIR, ARTIFACTS_MANAGEMENT_DIR, ARTIFACTS_INFERENCE_DIR, ARTIFACTS_INCRSD_TIMEOUT_INFERENCE_DIR, ARTIFACTS_HTTPS_DIR] :
+    ALL_DIRS = [MODEL_STORE_DIR, ARTIFACTS_MANAGEMENT_DIR, ARTIFACTS_INFERENCE_DIR, ARTIFACTS_INCRSD_TIMEOUT_INFERENCE_DIR,
+    ARTIFACTS_HTTPS_DIR, ARTIFACTS_MANAGEMENT_DIR_KF, ARTIFACTS_INFERENCE_DIR_KF, ARTIFACTS_INCRSD_TIMEOUT_INFERENCE_DIR_KF, ARTIFACTS_HTTPS_DIR_KF] 
+    for DIR in ALL_DIRS:
         shutil.rmtree(DIR, True)
         os.makedirs(DIR, exist_ok=True)
 
@@ -144,6 +176,7 @@ def test_api(collection):
         "inference_kf": trigger_inference_tests_kf,
         "increased_timeout_inference": trigger_incr_timeout_inference_tests,
         "https": trigger_https_tests,
+        "https_kf": trigger_https_tests_kf,
         "all": trigger_all
     }
 
